540babf954e311a5ade5a9225be57c1118e4bb1c
hilalkartal <hikartallal@gmail.com>
2025-11-15 12:18:15 +0300

    initial springboot setup

---
+++ src/test/java/com/marvin/campustrade/CampusTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvin.campustrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
