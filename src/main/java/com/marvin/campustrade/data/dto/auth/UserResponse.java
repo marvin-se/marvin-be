89be49913765a07290719c936db473e9158b08f4
zmbekdemir <zelihamelek.2004@gmail.com>
2025-12-03 00:01:29 +0300

    Change the return type of service to dto and extend the returned dto

--- src/main/java/com/marvin/campustrade/data/dto/auth/UserResponse.java
+++ src/main/java/com/marvin/campustrade/data/dto/auth/UserResponse.java
@@ -2,11 +2,15 @@ package com.marvin.campustrade.data.dto.auth;
 
 import com.marvin.campustrade.data.entity.University;
 
+import java.time.LocalDateTime;
+
 public record UserResponse(
         Long id,
         String fullName,
         String email,
         String phoneNumber,
-        University universityName,
-        String profilePicUrl
+        University university,
+        String profilePicUrl,
+        LocalDateTime createdAt,
+        Boolean isActive
 ) {}
--- src/main/java/com/marvin/campustrade/service/UserService.java
+++ src/main/java/com/marvin/campustrade/service/UserService.java
@@ -1,8 +1,9 @@
 package com.marvin.campustrade.service;
 
 import com.marvin.campustrade.data.dto.auth.RegisterRequest;
+import com.marvin.campustrade.data.dto.auth.UserResponse;
 import com.marvin.campustrade.data.entity.Users;
 
 public interface UserService {
-    Users createUser(RegisterRequest request);
+    UserResponse createUser(RegisterRequest request);
 }
