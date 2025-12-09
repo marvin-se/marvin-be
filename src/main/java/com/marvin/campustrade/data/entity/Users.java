deb443c1b7641454fdcd721b8b413ec1035c360c
hilalkartal <hikartallal@gmail.com>
2025-12-10 00:31:34 +0300

    users description column

--- src/main/java/com/marvin/campustrade/data/entity/Users.java
+++ src/main/java/com/marvin/campustrade/data/entity/Users.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE USER SET IS_ACTIVE = false WHERE ID = ?")
+@SQLDelete(sql = "UPDATE USERS SET IS_ACTIVE = false WHERE ID = ?")
 @Where(clause = "IS_ACTIVE = true")
 public class Users {
     @Id
