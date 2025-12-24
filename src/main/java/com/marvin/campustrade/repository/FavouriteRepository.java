07a1ce8e76a6be4511cc6c1581ee749ebb4f3ac6
harundmrpnce <harundemirpence@gmail.com>
2025-12-24 15:16:48 +0300

    adjust listing functions to decrease response time

--- src/main/java/com/marvin/campustrade/repository/FavouriteRepository.java
+++ src/main/java/com/marvin/campustrade/repository/FavouriteRepository.java
@@ -4,11 +4,19 @@ import com.marvin.campustrade.data.entity.Favourite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.marvin.campustrade.data.entity.Users;
 import com.marvin.campustrade.data.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface FavouriteRepository extends JpaRepository<Favourite,Long> {
+    @Query("""
+        select f.product.id
+        from Favourite f
+        where f.user.id = :userId
+    """)
+    Set<Long> findFavouriteProductIdsByUserId(Long userId);
 
     boolean existsByUserAndProduct(Users user, Product product);
 
